b0fb9214e4f661da6d5b8e9bfffaeaa30cd452fc
Nicholas Wolf <nicholas.wolf@nyu.edu>
2021-05-27 13:19:30 -0400

    Updating batch script

--- crnn/bb-handwriting.s
+++ crnn/bb-handwriting.s
@@ -3,19 +3,19 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=70:00:00
-#SBATCH --mem=32GB
+#SBATCH --mem=2GB
 #SBATCH --job-name=brownbros_handwriting
 #SBATCH --mail-type=END
 #SBATCH --mail-user=nmw2@nyu.edu
 #SBATCH --output=brownbros_handwriting_%j.out
 
 module purge;
-module load anaconda3/5.3.1;
+module load anaconda3/2020.07;
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK;
 
-source /share/apps/anaconda3/5.3.1/etc/profile.d/conda.sh;
-conda activate ./penv;
-export PATH=./penv/bin:$PATH;
+source /share/apps/anaconda3/2020.07/etc/profile.d/conda.sh;
+conda activate ../penv;
+export PATH=../penv/bin:$PATH;
 
 python3  main.py -r validated -f 2021-04-28_transcriptions_report.csv -e 60

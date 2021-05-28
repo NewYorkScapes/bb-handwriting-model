f6571371e0999404df6fea6f4bff75f0590f7193
Nicholas Wolf <nmw2@log-2.nyu.cluster>
2021-05-28 16:35:33 -0400

    Lates slurm batch v

--- crnn/bb-handwriting.s
+++ crnn/bb-handwriting.s
@@ -3,11 +3,12 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=70:00:00
-#SBATCH --mem=2GB
+#SBATCH --mem=16GB
 #SBATCH --job-name=brownbros_handwriting
 #SBATCH --mail-type=END
 #SBATCH --mail-user=nmw2@nyu.edu
 #SBATCH --output=brownbros_handwriting_%j.out
+#SBATCH --array=0-4
 
 module purge;
 module load anaconda3/2020.07;
@@ -18,5 +19,9 @@ source /share/apps/anaconda3/2020.07/etc/profile.d/conda.sh;
 conda activate /scratch/nmw2/bb-handwriting-model/penv;
 export PATH=/scratch/nmw2/bb-handwriting-model/penv/bin:$PATH;
 
-python3 /scratch/nmw2/bb-handwriting-model/crnn/main.py -r 
-validated_onepass -f 2021-05-28_transcriptions_report.csv -e 60
+runopts=(validated validated_onepass validated_iam validated_onepass_iam 
+onepass_iam)
+
+rtype=${runopts[$SLURM_ARRAY_TASK_ID]}
+
+python3 /scratch/nmw2/bb-handwriting-model/crnn/main.py -r $rtype -f 2021-05-28_transcriptions_report.csv -e 60
